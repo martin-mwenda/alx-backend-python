a5e886a06a038b656f5e2d971b13bdb489f8cc49
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-10-12 18:52:21 +0000

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Define and annotate variables"""
+
+
+a: int = 1  # an integer with a value of 1
+pi: float = 3.14  # a float with a value of 3.14
+i_understand_annotations: bool = True  # a boolean with a value of True
+school: str = "Holberton"  # a string with a value of "Holberton"
