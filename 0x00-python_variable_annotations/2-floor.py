4675b40814959b873a5f8269868c9e2b47cac46b
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-10-12 18:34:08 +0000

    a floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" The floor function rounds down the input float to the nearest
+integer less than or equal to the given number."""
+
+
+def floor(n: float) -> float:
+    """
+    Returns the floor of the given float.
+
+    Args:
+    n (float): The input float.
+
+    Returns:
+    float: The floor value of the input float.
+    """
+    return math.floor(n)
