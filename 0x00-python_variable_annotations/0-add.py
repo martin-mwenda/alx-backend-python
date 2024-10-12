14d9c3f60e12116e646f8bef109e6a974dba21d0
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-10-12 17:51:00 +0000

    Basic annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,12 +4,9 @@
 def add(a: float, b: float) -> float:
     """
     Adds two float numbers and returns their sum.
-
-    Args:
-    a (float): The first number.
-    b (float): The second number.
-
-    Returns:
-    float: The sum of a and b.
+    
+    :param a : The first number.
+    :param b: The second number.
+    Returns: The sum of a and b.
     """
     return a + b
